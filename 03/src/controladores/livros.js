e32c823ccc43f9533c7917153c6b1cf19c1b2a86
tiago-tsm <tiago.miranda@tsmdebug.com.br>
2023-09-24 23:13:32 -0300

    incluindo exercicios 3

--- 03/src/controladores/livros.js
+++ 03/src/controladores/livros.js
@@ -1,4 +1,4 @@
-let { livros } = require("../bancodedados");
+let { livros, identificadorLivro } = require("../bancodedados");
 
 
 const listaDeLivros = (req, res) => {
@@ -9,20 +9,135 @@ const listaDeLivros = (req, res) => {
 const obterLivros = (req, res ) => {
 
     const { id } = req.params;
+    const { status } = req.body;
+        
 
 
     const livro = livros.find((livro) =>{
         return livro.id === Number(id);
+    
     });
+
+    if (livro.status === false) {
+        return res.status(200).json({ mensagem: "Livro alugado" });
+    }
 
     if(!livro){
         return res.status(404).json({mensagem: "Livro não Encontrado"});
     }
 
+    
+    
+   
     return res.status(200).json(livro);
+
+
+    
 
 };
 
+
+const cadastrarLivros = (req, res) =>{
+    const { titulo, autor, ano ,numPaginas } = req.body;
+
+
+if (!titulo) {
+    return res.status(400).json({ mensagem: 'O titulo é obrigatório' });
+}
+
+if (!autor) {
+
+    return res.status(400).json ({ mensagem: 'O autor é obrigatorio'})    
+}
+if (!ano) {
+
+    return res.status(400).json ({ mensagem: 'O ano é obrigatorio'})    
+}
+if (!numPaginas) {
+
+    return res.status(400).json ({ mensagem: 'O numero de paginas  é obrigatorio'})    
+}
+
+const livro = {
+    id: identificadorLivro++,
+    titulo,
+    autor,
+    ano,
+    numPaginas
+}
+
+livros.push(livro);
+return res.status(201).json(livros)
+
+}
+
+
+const atualizarUmLivro = (req, res) =>{
+    const { id } = req.params;
+    const {titulo, autor, ano ,numPaginas } = req.body;
+
+    if (!titulo) {
+        return res.status(400).json({ mensagem: 'O titulo é obrigatório' });
+    }
+    
+    if (!autor) {
+    
+        return res.status(400).json ({ mensagem: 'O autor é obrigatorio'});    
+    }
+    if (!ano) {
+    
+        return res.status(400).json ({ mensagem: 'O ano é obrigatorio'});   
+    }
+    if (!numPaginas) {
+    
+        return res.status(400).json ({ mensagem: 'O numero de paginas  é obrigatorio'});    
+    }
+
+    const livro = livros.find((livro) =>{
+        return livro.id === Number(id);
+    });
+
+    if (!livro) {
+        return res.status(404).json({ mensagem: 'Livro  não encontrado.' });
+    }
+
+    
+
+    livro.titulo = titulo;
+    livro.autor = autor;
+    livro.ano = ano;
+    livro.numPaginas = numPaginas;
+
+    res.json({ mensagem: "Livro substituido" });
+
+ 
+
+}
+
+const atualizarStatusDoLivro =  (req, res) =>{
+    const {id} = req.params;
+    const {status} = req.body;
+
+    const livro = livros.find((livro) =>{
+        return livro.id === Number(id);
+    });
+
+
+   
+
+    if (!livro) {
+        return res.status(404).json({ mensagem: 'Livro  não encontrado.' });
+    }
+
+  
+
+
+
+    livro.status = status;
+    return res.status(200).send()
+
+
+}
 
 
 
@@ -32,6 +147,13 @@ const obterLivros = (req, res ) => {
 
 module.exports = {
     listaDeLivros,
-    obterLivros
+    obterLivros,
+    cadastrarLivros,
+    atualizarUmLivro,
+    atualizarStatusDoLivro
     
 };
+
+
+
+
--- 03/src/rotas.js
+++ 03/src/rotas.js
@@ -13,7 +13,9 @@ const rotas = express();
 
 rotas.get('/livros', livros.listaDeLivros);
  rotas.get('/livros/:id', livros.obterLivros);
-// rotas.post('/convidados',convidados.adicionarConvidado);
+ rotas.post('/livros',livros.cadastrarLivros);
+ rotas.put('/livros/:id', livros.atualizarUmLivro);
+ rotas.patch('/livros/:id/status', livros.atualizarStatusDoLivro);
 // rotas.delete('/convidados/:nome', convidados.excluirConvidado);
 
 
